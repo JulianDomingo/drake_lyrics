937b5c088847b6ad9855709d02c37a8e352b89d1
JulianDomingo <julian.domingo@utexas.edu>
2018-04-27 00:08:11 -0500

    Updated spotify scraper to scrape singles songs, generated trainable data for the char rnn.

--- scrapers/spotify_scraper.py
+++ scrapers/spotify_scraper.py
@@ -16,7 +16,12 @@ track_names = set()
 
 # Auth stuff
 try:
-    token = util.prompt_for_user_token(username)
+    # You'll have to replace all the parameters passed to prompt_for_user_token() 
+    # in order to correctly receive a token, except the redirect_uri which can be left alone.
+    token = util.prompt_for_user_token(username, 
+                                       client_id='001d00cea86f45918349dd8d039fbd5a',
+                                       client_secret='fbd0dbe61cd7462e8f995288c3010090',
+                                       redirect_uri='http://google.com')
 except: 
     os.remove(".cache-{}".format(username))
     token = util.prompt_for_user_token(username)
@@ -32,7 +37,7 @@ artist_id = artist['id']
 # Fetch all Drake albums using his ID.
 album_results = sp.artist_albums(artist_id)['items']
 
-# Retrieve all track names from each album.
+# Retrieve all track names from each "regular" album.
 for album in album_results:
     album_name = album['name']
 
@@ -69,8 +74,53 @@ for album in album_results:
         i += 1
 
 
-print ("Total songs found: {}".format(len(track_names)))
+print ("Total album songs found: {}".format(len(track_names)))
 
+
+# Retrieve all track names from each "single" album.
+singles_results = sp.artist_albums(artist_id, album_type='single')['items']
+singles_found = 0
+singles_seen = set()
+
+for single in singles_results:
+    single_name = single['name']
+
+    if single_name in singles_seen:
+        continue
+
+    singles_seen.add(single_name)
+
+    print ("Retrieving all song names from single: '{}'...".format(single_name))
+
+    single_id = single['id']
+
+    # Extract track(s) from the single. 
+    track_results = sp.album_tracks(single_id)['items']
+
+    i = 0
+
+    # Add each track to
+    for track in track_results:
+        track_name = track['name']
+
+        # Removing unused characters in metrolyrics URL 
+        track_name = track_name.replace('&', '') \
+                               .replace('/', '') \
+                               .replace('\'', '') \
+                               .replace(',', '') \
+                               .replace('?', '') \
+                               .replace('-', '') \
+                               .replace('’', '') \
+                               .replace('Album Version (Edited)', '')
+
+        track_name = "-".join(track_name.lower().split())
+        track_names.add(track_name)
+
+        i += 1
+        singles_found += 1
+
+
+print ("Total singles found: {}".format(int(singles_found)))
 
 # Fetch the lyrics for each track name.
 quote_page_metro = 'http://metrolyrics.com/{}-lyrics-drake.html'
@@ -81,12 +131,14 @@ filename = '../data/drake-songs-spotify.csv'
 # providing the correct naming convention or scraping on genius.com.
 
 # Handle problematic songs which still exist in metrolyrics.
-track_names.difference_update({"karaoke", "star67", "cameras-good-ones-go-interlude-medley", "buried-alive-interlude"})
-track_names.update({"cameras-good-ones-go-interlude", "marvins-room-buried-alive-interlude"})
+track_names.difference_update({"karaoke", "star67", "cameras-good-ones-go-interlude-medley", "buried-alive-interlude", "no-frauds", "look-alive-(feat.-drake)"})
+track_names.update({"cameras-good-ones-go-interlude", 
+                    "marvins-room-buried-alive-interlude"})
 dictionary = {"song_name": list(track_names)}
 metro_songs = pd.DataFrame(data=dictionary)
 
 for index, track in enumerate(track_names):
+    print("Saving the lyrics for '{}'...".format(track))
     page = urllib.request.urlopen(quote_page_metro.format(track))
 
     soup = BeautifulSoup(page, 'html.parser')
@@ -105,8 +157,6 @@ for index, track in enumerate(track_names):
 
     metro_songs.at[index, 'lyrics'] = lyrics
 
-    print("Saving the lyrics for '{}'...".format(track))
-
 
 # Scrape songs from genius.com which don't exist in metrolyrics.
 quote_page_genius = 'http://genius.com/Drake-{}-lyrics'
@@ -120,7 +170,6 @@ for index, track in enumerate(dictionary["song_name"]):
     soup = BeautifulSoup(page, "html.parser") 
     verses = soup.find('div', class_='lyrics').text.strip()
     verses = verses.splitlines()
-    print(type(verses))
 
     lyrics = ''
 
